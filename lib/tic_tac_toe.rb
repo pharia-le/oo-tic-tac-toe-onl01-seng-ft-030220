aaedf8211ba31ee26e701d3ca7a0afee335b860d
Pharia Le <pharia.le@gmail.com>
2020-03-15 15:11:56 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,103 @@
+require 'pry'
+
+class TicTacToe
+  
+  attr_accessor :board
+  
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+    ]
+  
+  def initialize
+    @board = Array.new(9, " ")
+  end
+  
+  def display_board
+    @board.each_with_index do |space, i|
+      if i % 3 == 0
+        print " #{space} "
+      elsif i == 1 || i == 4 || i == 7
+        print "| #{space} |"
+      else
+        print " #{space} \-----------n"
+      end
+    end
+  end
+  
+  def input_to_index(input)
+    input.to_i - 1
+  end
+  
+  
+  def move(index, token = "X")
+    @board[index] = token
+  end
+  
+  
+  def position_taken?(position)
+    @board[position] != " "
+  end
+  
+  def valid_move?(position)
+    position >= 0 && position < 9 && !position_taken?(position)
+  end
+  
+  def turn 
+    input = gets.chomp
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
+    else
+      turn
+    end
+  end
+  
+  def turn_count
+    @board.count {|space| space != " "}
+  end
+  
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+  
+  def won?
+    x_spaces = []
+    o_spaces = []
+
+    board.each_with_index do |space, i|
+      if space == "X"
+        x_spaces << i
+      elsif space == "O"
+        o_spaces << i
+      end
+    end
+    WIN_COMBINATIONS.select { |win| (win & x_spaces == x_spaces) || (win & o_spaces == o_spaces)
+  end
+  
+
+  
+  def full?
+    
+  end
+  
+  def draw?
+    
+  end
+  
+  def over?
+    
+  end
+  
+  def player
+    
+  end
+
+end
